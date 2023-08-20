75ee1042e4b274cc7a4682c6a21ef82151e5028c
FreddieCrew <120303127+FreddieCrew@users.noreply.github.com>
2023-08-19 23:33:15 -0300

    Added github link in the footer that i forgot

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-white text-dark p-4 mt-8 text-center">
-      © {new Date().getFullYear()} John Michael Reyes/JustJohn | Content stole from <a href='https://www.textnot.pictures/' target='_blank'>textnot.pictures</a> | Idea stole from <a href='https://nohello.net/' target='_blank'>nohello.net</a>.
+      © {new Date().getFullYear()} <a href='https://www.github.com/FreddieCrew' target='_blank'>John Michael Reyes/JustJohn</a> | Content stole from <a href='https://www.textnot.pictures/' target='_blank'>textnot.pictures</a> | Idea stole from <a href='https://nohello.net/' target='_blank'>nohello.net</a> |       <a href='https://github.com/FreddieCrew/usetextnotpictures' target='_blank'>See the source code on Github</a>
+.
     </footer>
   );
 };
